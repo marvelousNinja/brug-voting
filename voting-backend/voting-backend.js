89e124fdaed46b6903e1229714a2810aae3514df
Alexander Sologub <alexander.sologub@rightscale.com>
2015-02-21 04:01:50 +0300

    Voting: Seeds

--- voting-backend/voting-backend.js
+++ voting-backend/voting-backend.js
@@ -1,2 +1,33 @@
 this.VotingVariants = new Meteor.Collection('votingVariants');
 
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    if (VotingVariants.find({}).count() === 0) {
+      _.map([{
+        name: 'Deployment',
+        votes: 0
+      }, {
+        name: 'Database',
+        votes: 0
+      }, {
+        name: 'Security',
+        votes: 0
+      }, {
+        name: 'Testing',
+        votes: 0
+      }, {
+        name: 'Show us the sources!',
+        votes: 0
+      }, {
+        name: 'Package system',
+        votes: 0
+      }, {
+        name: 'Pitfalls',
+        votes: 0
+      }, {
+        name: 'Reactive Cat Generator',
+        votes: 0
+      }], function(params) { VotingVariants.insert(params) });
+    }
+  });
+}
